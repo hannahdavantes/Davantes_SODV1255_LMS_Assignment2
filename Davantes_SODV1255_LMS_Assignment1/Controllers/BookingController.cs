d9fdffc172dd75be4cfb8d8a9d3cd53e11b4e896
Hannah Davantes <hannahdavantes@yahoo.com>
2026-01-30 23:32:35 -0700

    Add bookings controller and model

---
+++ Davantes_SODV1255_LMS_Assignment1/Controllers/BookingController.cs
@@ -0,0 +1,47 @@
+﻿using Davantes_SODV1255_LMS_Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Davantes_SODV1255_LMS_Assignment1.Controllers {
+    [ApiController]
+    [Route("/bookings")]
+    public class BookingController : Controller {
+
+        //GET /bookings
+        [HttpGet]
+        public IActionResult GetAllBookings() {
+            List<Booking> bookings = new List<Booking>() {
+                new Booking(1, 1, 1),
+                new Booking(2, 2, 3),
+                new Booking(3, 3, 2)
+            };
+            return Json(bookings);
+        }
+
+        //GET /bookings/1
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id) {
+            Booking booking = new Booking(id, 1, 1);
+            return Json(booking);
+        }
+
+        //POST /bookings
+        [HttpPost]
+        public IActionResult CreateBooking() {
+            Booking newBooking = new Booking(4, 2, 3);
+            return Json(new { message = "New booking created", booking = newBooking });
+        }
+
+        //PUT /bookings/1
+        [HttpPut("{id}")]
+        public IActionResult UpdateBooking(int id) {
+            Booking editBooking = new Booking(id, 2, 3);
+            return Json(new { message = $"Booking with ID of {editBooking.ID} is updated", booking = editBooking });
+        }
+
+        //DELETE /bookings/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id) {
+            return Json(new { message = $"Booking with ID of {id} is deleted" });
+        }
+    }
+}
---
+++ Davantes_SODV1255_LMS_Assignment1/Models/Booking.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Davantes_SODV1255_LMS_Assignment1.Models {
+    public class Booking {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "BookID is required")]
+        public int BookID { get; set; }
+
+        [Required(ErrorMessage = "ReaderID is required")]
+        public int ReaderID { get; set; }
+
+        [Required(ErrorMessage = "Booking date is required")]
+        public DateTime BookingDate { get; set; }
+
+        [Required(ErrorMessage = "Due date is required")]
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        [StringLength(20, ErrorMessage = "Status cannot exceed 20 characters")]
+        public string Status { get; set; }
+
+        public Booking() {
+        }
+
+        public Booking(int id, int bookID, int readerID) {
+            ID = id;
+            BookID = bookID;
+            ReaderID = readerID;
+            BookingDate = DateTime.Now;
+            DueDate = BookingDate.AddDays(14);
+            ReturnDate = null;
+            Status = "Borrowed"; //Returned, Overdue
+        }
+    }
+}
